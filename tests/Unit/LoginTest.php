496d4fbf547a5336ebb85f1078de54b2a67f0229
Diego Momotic <diegomomotic@gmail.com>
2018-09-19 16:40:15 -0600

    Agregue mas pruebas a login y cree algunas pruebas para el logout

--- tests/Unit/LoginTest.php
+++ tests/Unit/LoginTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
 
 class LoginTest extends TestCase
 {
@@ -19,7 +20,6 @@ class LoginTest extends TestCase
             'email' => 'customer1@mail.com',
             'password' => '123456',
         ]);
-
         $response->assertSessionHasNoErrors();
     }
 
@@ -34,7 +34,6 @@ class LoginTest extends TestCase
             'email' => 'badcustomer@mail.com',
             'password' => 'badpassword',
         ]);
-
         $response->assertSessionHasErrors();
     }
 
@@ -49,8 +48,43 @@ class LoginTest extends TestCase
             'email' => 'customer1@mail.com',
             'password' => '123456',
         ]);
-
         $response->assertRedirect('/');
     }
 
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function it_validates_the_login_page_loads_successful()
+    {
+        $response = $this->get('/login');
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function it_validates_a_user_cannot_see_a_login_form_when_authenticated()
+    {
+        $user = User::find(1);
+        $response = $this->actingAs($user)->get('/login');
+        $response->assertRedirect('/');
+    }
+
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function it_validates_a_login_user_can_see_a_log_in_opcion()
+    {
+        $response = $this->get('/');
+        $response->assertSuccessful();
+        $response->assertSee('Iniciar Sesion');
+    }
+    
 }
