2d838f86b5136551d4fcb18afc370eac663b44d2
Esteban Alvarez <davix93a@gmail.com>
2018-09-19 17:54:02 -0600

    Pruebas unitarias dedicadas a la visualizacion del producto existente y no existente - 201313872

--- tests/Unit/ProductViewTest.php
+++ tests/Unit/ProductViewTest.php
@@ -10,11 +10,26 @@ class ProductViewTest extends TestCase
 {
     /**
      * A basic test example.
-     *
+     * @test
      * @return void
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
+    public function if_product_exists()
+    {	
+    	$response = $this->get('/ProductView/1');
+        $response->assertSee('Comprar');
+        $response->assertSuccessful();
+    }
+
+    /**
+     * A basic test example.
+     * @test
+     * @return void
+     */
+    public function if_product_doesnt_exists()
+    {	
+    	$response = $this->get('/ProductView/321');
+    	$response->assertSee('PRODUCTO NO ENCONTRADO');
+        $response->assertSee('Regresar');
+        $response->assertSuccessful();
     }
 }
