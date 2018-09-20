cef39cb5537cc7742dc1eaea578a195c2c34598b
Marellyn <mlis.trejo@gmail.com>
2018-09-20 11:51:40 -0600

    interfaz terminada - 201313688

--- resources/views/DashoardClient.blade.php
+++ resources/views/DashoardClient.blade.php
@@ -1,25 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-
-        <!-- Home -->
-        <div id="home" class="hero-area">
+<!-- Hero-area -->
+        <div class="hero-area section">
 
             <!-- Backgound Image -->
-            <div class="bg-image bg-parallax overlay" style="background-image:url(./img/home-background.jpg)"></div>
+            <div class="bg-image bg-parallax overlay" style="background-image:url(./img/page-background.jpg)"></div>
             <!-- /Backgound Image -->
 
-            <div class="home-wrapper">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <h1 class="white-text">Dashoard Cliente</h1>
-                        </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 text-center">
+                        <h1 class="white-text">Bienvenido</h1>
+
                     </div>
                 </div>
             </div>
 
         </div>
-        <!-- /Home -->
-    
+        <!-- /Hero-area -->
+        <hr>
+        <hr>
+        <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 text-center">
+            <div class="service-box mt-5 mx-auto">
+              
+              <a href="#search">
+                <i class="fa fa-book fa-5x fa-lg"></i>
+                <br>
+                <br>
+                <p class="text-muted mb-0">Ver Productos </p>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 text-center">
+            <div class="service-box mt-5 mx-auto">
+              
+              <a href="#search">
+                <i class="fa fa-search fa-5x fa-lg"></i>
+                <br>
+                <br>
+                <p class="text-muted mb-0">Buscar Productos</p>
+              </a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-4 text-center">
+            <div class="service-box mt-5 mx-auto">
+              
+              <a href="#search">
+                <i class="fa fa-cart-arrow-down fa-5x fa-lg"></i>
+                <br>
+                <br>
+                <p class="text-muted mb-0">Ver Historial de Compras</p>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
 @endsection
