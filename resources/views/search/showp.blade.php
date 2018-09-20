30c4ce6d010065f1a69b9f4de05411a2a00cdb40
salaz310 <lrso.10@hotmail.com>
2018-09-20 15:19:05 -0500

    Se agrego el recibir el ID del producto para mostrarlo-201213181

--- resources/views/search/showp.blade.php
+++ resources/views/search/showp.blade.php
@@ -34,7 +34,7 @@
 	          <div class="col-lg-4 col-md-6 text-center">
 	            <div class="service-box mt-5 mx-auto">
 	              <i class="fa fa-4x fa-gift mb-3 sr-icons" style="color:navy" ></i>
-                <!--<a href="{{ url('/ProductView/'.$product->id) }}">-->
+                <a href="{{ url('/ProductView/'.$product->id) }}">
 	              <p style="color:white; background: black;">{{ $product->name }}</p>
 	              </a>
 	            </div>
